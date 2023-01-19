3f5c673f2560b111616d1ed2bb0e5f0d13a737f5
scottc11 <scott.m.campbell11@gmail.com>
2023-01-19 07:53:53 -0500

    implemented SVF filter class

--- MultiEffects/main.cpp
+++ MultiEffects/main.cpp
@@ -11,6 +11,9 @@ using namespace patch_sm;
 #define POT_D CV_2
 
 DaisyPatchSM patch;
+Parameter cutoff_ctrl, res_ctrl, drive_ctrl;
+Svf svf;
+
 
 void __attribute__((optimize("O0"))) AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
@@ -19,14 +22,28 @@ void __attribute__((optimize("O0"))) AudioCallback(AudioHandle::InputBuffer in, 
     float output_level = patch.GetAdcValue(POT_A);
     float gain = patch.GetAdcValue(POT_C) * 100 + 1.2;
 
+    // get new control values
+    float cutoff = cutoff_ctrl.Process();
+    float res = res_ctrl.Process();
+    float drive = drive_ctrl.Process();
+
+    svf.SetFreq(cutoff);
+    svf.SetRes(res);
+    svf.SetDrive(drive);
+
     for(size_t i = 0; i < size; i++)
     {
-        for (int chan = 0; chan < 2; chan++)
-        {
-            float input = in[chan][i] * output_level;
-            float output = input * gain;
-            out[chan][i] = SoftClip(output) + input;
-        }
+        svf.Process(in[0][i]);
+        // for (int chan = 0; chan < 2; chan++)
+        // {
+        //     float input = in[chan][i] * output_level;
+        //     float output = input * gain;
+        //     out[chan][i] = svf.Low();
+        // }
+        out[0][i] = svf.Low();
+        out[1][i] = svf.High();
+        out[2][i] = svf.Band();
+        out[3][i] = svf.Notch();
     }
 }
 
@@ -36,6 +53,13 @@ int main(void)
     float sampleRate;
     patch.Init();
     sampleRate = patch.AudioSampleRate();
+
+    svf.Init(sampleRate);
+    
+    // setup controls
+    cutoff_ctrl.Init(patch.controls[POT_C], 20, 20000, Parameter::LOGARITHMIC);
+    res_ctrl.Init(patch.controls[POT_B], .3, 1, Parameter::LINEAR);
+    drive_ctrl.Init(patch.controls[POT_D], .3, 1, Parameter::LINEAR);
 
     patch.StartAdc();
     patch.StartAudio(AudioCallback);
